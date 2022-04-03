de97c2ca9b0dba8328d66e86927e172bb2410c7b
Farukh Aleem <farrukhaleem31@gmail.com>
2022-04-04 04:54:40 +0500

    Display Posts

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 if(localStorage.getItem('token')){
   setAuthToken(localStorage.getItem('token'))
@@ -84,6 +85,14 @@ const App = () => {
             element={
               <PrivateRoute>
                 <AddEducation />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path='/posts'
+            element={
+              <PrivateRoute>
+                <Posts />
               </PrivateRoute>
             }
           />
